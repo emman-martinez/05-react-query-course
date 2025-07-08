080cc06f0539b34b093cec77e3e03503c6c110b9
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-07-08 11:40:04 -0600

    Implement user data fetching in IssueItem component

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -1,6 +1,7 @@
 import { GoIssueClosed, GoIssueOpened, GoComment } from "react-icons/go";
 import { Link } from "react-router-dom";
 import { relativeDate } from "../helpers/relativeDate";
+import { useUserData } from "../helpers/useUserData";
 
 export const IssueItem = ({
   assignee,
@@ -11,36 +12,50 @@ export const IssueItem = ({
   number,
   status,
   title,
-}) => (
-  <li>
-    <div>
-      {status === "done" || status === "cancelled" ? (
-        <GoIssueClosed style={{ color: "red" }} />
-      ) : (
-        <GoIssueOpened style={{ color: "green" }} />
+}) => {
+  const assigneeUser = useUserData(assignee);
+  const createdByUser = useUserData(createdBy);
+
+  return (
+    <li>
+      <div>
+        {status === "done" || status === "cancelled" ? (
+          <GoIssueClosed style={{ color: "red" }} />
+        ) : (
+          <GoIssueOpened style={{ color: "green" }} />
+        )}
+      </div>
+      <div className="issue-content">
+        <span>
+          <Link to={`/issues/${number}`}>{title}</Link>
+          {labels.map((label) => (
+            <span key={label} className={`label red`}>
+              {label}
+            </span>
+          ))}
+        </span>
+        <small>
+          # {number} opened {relativeDate(createdDate)}{" "}
+          {createdByUser.isSuccess && `by ${createdByUser.data?.name}`}
+        </small>
+      </div>
+      {assignee && (
+        <img
+          alt={`Assigned to ${
+            assigneeUser.isSuccess ? assigneeUser.data.name : "avatar"
+          }`}
+          className="assigned-to"
+          src={assigneeUser.isSuccess && assigneeUser.data.profilePictureUrl}
+        />
       )}
-    </div>
-    <div className="issue-content">
-      <span>
-        <Link to={`/issues/${number}`}>{title}</Link>
-        {labels.map((label) => (
-          <span key={label} className={`label red`}>
-            {label}
-          </span>
-        ))}
+      <span className="comment-count">
+        {commentCount > 0 && (
+          <>
+            <GoComment />
+            {commentCount}
+          </>
+        )}
       </span>
-      <small>
-        # {number} opened {relativeDate(createdDate)} by {createdBy}
-      </small>
-    </div>
-    {assignee && <div>{assignee}</div>}
-    <span className="comment-count">
-      {commentCount > 0 && (
-        <>
-          <GoComment />
-          {commentCount}
-        </>
-      )}
-    </span>
-  </li>
-);
+    </li>
+  );
+};
