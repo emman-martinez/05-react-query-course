4695ab4bdcb1c5be2084436339942bfcd70fcc77
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-07-18 14:56:38 -0600

    12-fetching-states

---
+++ src/components/FetchingIndicator.jsx
@@ -0,0 +1,16 @@
+import { useIsFetching } from "react-query";
+import Loader from "./Loader";
+
+const FetchingIndicator = () => {
+  const isFetching = useIsFetching();
+
+  if (!isFetching) return null;
+
+  return (
+    <div className="fetching-indicator">
+      <Loader />
+    </div>
+  );
+};
+
+export default FetchingIndicator;
--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
 import fetchWithError from "../helpers/fetchWithError";
+import Loader from "./Loader";
 
 export default function IssuesList({ labels, status }) {
   const [searchValue, setSearchValue] = useState("");
@@ -47,7 +48,7 @@ export default function IssuesList({ labels, status }) {
           onChange={onChange}
         />
       </form>
-      <h2>Issues List</h2>
+      <h2>Issues List {issuesQuery.isFetching && <Loader />}</h2>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
       ) : issuesQuery.isError ? (
