4817bcb0dda764eff95fd3bdc09de746de064f0c
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-07-25 14:56:50 -0600

    13-preloading-data

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { relativeDate } from "../helpers/relativeDate";
 import { useUserData } from "../helpers/useUserData";
 import { Label } from "./Label";
+import { useQueryClient } from "react-query";
+import fetchWithError from "../helpers/fetchWithError";
 
 export const IssueItem = ({
   assignee,
@@ -16,9 +18,20 @@ export const IssueItem = ({
 }) => {
   const assigneeUser = useUserData(assignee);
   const createdByUser = useUserData(createdBy);
+  const queryClient = useQueryClient();
 
   return (
-    <li>
+    <li
+      onMouseEnter={() => {
+        queryClient.prefetchQuery(["issues", number.toString()], () =>
+          fetchWithError(`/api/issues/${number}`)
+        );
+        queryClient.prefetchQuery(
+          ["issues", number.toString(), "comments"],
+          () => fetchWithError(`/api/issues/${number}/comments`)
+        );
+      }}
+    >
       <div>
         {status === "done" || status === "cancelled" ? (
           <GoIssueClosed style={{ color: "red" }} />
--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { IssueItem } from "./IssueItem";
 import fetchWithError from "../helpers/fetchWithError";
 import Loader from "./Loader";
 
 export default function IssuesList({ labels, status }) {
+  const queryClient = useQueryClient();
   const [searchValue, setSearchValue] = useState("");
 
-  const issuesQuery = useQuery(["issues", { labels, status }], ({ signal }) => {
-    const statusString = status ? `&status=${status}` : "";
-    const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
+  const issuesQuery = useQuery(
+    ["issues", { labels, status }],
+    async ({ signal }) => {
+      const statusString = status ? `&status=${status}` : "";
+      const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
 
-    return fetchWithError(`/api/issues?${labelsString}${statusString}`, {
-      signal,
-    });
-  });
+      const results = await fetchWithError(
+        `/api/issues?${labelsString}${statusString}`,
+        {
+          signal,
+        }
+      );
+
+      results.forEach((issue) => {
+        queryClient.setQueryData(["issues", issue.number.toString()], issue);
+      });
+
+      return results;
+    }
+  );
 
   const searchQuery = useQuery(
     ["issues", "search", searchValue],
