98b8fd52c68e99d747fe0042a6540fd29da117fa
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-07-28 16:41:40 -0600

    14-adding-issues

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 import { StatusSelect } from "../components/StatusSelect";
+import { Link } from "react-router-dom";
 
 export default function Issues() {
   const [labels, setLabels] = useState([]);
@@ -28,6 +29,10 @@ export default function Issues() {
           <LabelList selected={labels} toggle={onToggle} />
           <h3>Status</h3>
           <StatusSelect onChange={onChangeStatus} value={status} />
+          <hr />
+          <Link className="button" to="/add">
+            Add Issue
+          </Link>
         </aside>
       </main>
     </div>
